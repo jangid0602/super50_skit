562bb8298a648f4c3ca4bda582a8014cfba76f8c
Aditi Agrawal <aditiagrawalsofficial@gmail.com>
2025-11-03 02:57:34 -0600

    Add files via upload

--- Task1.cpp
+++ Task1.cpp
@@ -1,204 +1,41 @@
 /*
  * @filename - Task1.cpp
- * @description - Performing operations using OOP Concepts.
+ * @description - This program performs count of subarray.
  * @author - Aditi Agrawal
  */
 
 #include <iostream>
-#include <bits/stdc++.h>
+#include <vector>
 using namespace std;
 
-// 1. The Efficient Traveler 
-int EfficientEnergyCalculation(vector<int>& energy, int initialEnergy) {
-    int n = energy.size();
-    int counted_city = 0;
-    for (int i = 0; i < n; i++) {
-        if (initialEnergy > 0 && initialEnergy >= energy[i]) {
-            counted_city++;
-            initialEnergy -= energy[i];
-        } else {
-            break;
-        }
-    }
-    return counted_city;
-}
-
-// 2.The Sum That Stands Out 
-int suffix_prefix(vector<int>& arr) {
-    int n = arr.size();
-    vector<int> prefix_sum(n, 0);
-    vector<int> suffix_sum(n, 0);
-
-    for (int i = 1; i < n; i++) prefix_sum[i] = prefix_sum[i - 1] + arr[i - 1];
-    for (int i = n - 2; i >= 0; i--) suffix_sum[i] = suffix_sum[i + 1] + arr[i + 1];
-
-    for (int i = 0; i < n; i++)
-        if (prefix_sum[i] == suffix_sum[i]) return i;
-
-    return -1;
-}
-
-// 3. Flip the Switch 
-int flips(vector<int>& switches) {
-    int zeros = 0, ones = 0;
-    for (int val : switches) {
-        if (val == 0)
-            zeros++;
-        else
-            ones++;
-    }
-    return min(zeros, ones);
-}
-
-// 4.  The Odd One Out 
-int OddOneOut(vector<int>& element) {
-    unordered_map<int, int> frequency;
-    for (int val : element) frequency[val]++;
-    for (auto& it : frequency)
-        if (it.second % 2 != 0) return it.first;
-    return -1;
-}
-
-// 5.  Smart Pair Finder 
-bool smartPairFinder(vector<int>& nums, int target) {
-    sort(nums.begin(), nums.end());
-    int left = 0, right = nums.size() - 1;
-    while (left < right) {
-        int sum = nums[left] + nums[right];
-        if (sum == target) return true;
-        else if (sum < target) left++;
-        else right--;
-    }
-    return false;
-}
-
-// 6. The Minimalist Painter 
-int MinimalistPainter(vector<int>& time) {
-    int n = time.size();
-    if (n <= 1) return 0;
-    int total_time = 0, max_time = 0;
-    for (int val : time) {
-        total_time += val;
-        max_time = max(max_time, val);
-    }
-    return total_time - max_time;
-}
-
-// helper function
-int getValidatedInt() {
-    int x;
-    cin >> x;
-    if (cin.fail()) {
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        throw invalid_argument("Invalid input! Please enter a number only.");
-    }
-    return x;
-}
-
-// ------------------------ MAIN FUNCTION ------------------------
 int main() {
-    while (true) {
-        try {
-            cout << "\nEnter choice from 1 to 6 to test different stories (0 to exit):\n";
-            cout << "1. Efficient Energy Calculation\n"
-                 << "2. Suffix Prefix Index Finder\n"
-                 << "3. Flip the Switch\n"
-                 << "4. The Odd One Out\n"
-                 << "5. Smart Pair Finder\n"
-                 << "6. Minimalist Painter\n"
-                 << "0. Exit\n";
-            cout<<"Enter your choice: ";
-            int choice = getValidatedInt();
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
 
-            if (choice == 0) {
-                cout << "Exiting program..." << endl;
-                break;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cout << "Enter element " << i + 1 << ": ";
+        cin >> arr[i];
+    }
+
+    int target;
+    cout << "Enter target sum: ";
+    cin >> target;
+
+    int count = 0;
+
+    for (int i = 0; i < n; i++) {
+        int sum = 0;
+        for (int j = i; j < n; j++) {
+            sum += arr[j];   
+            if (sum == target) {
+                count++;  
             }
-
-            switch (choice) {
-                case 1: {
-                    vector<int> energy;
-                    cout << "Enter number of cities: ";
-                    int n = getValidatedInt();
-                    cout << "Enter energy values: ";
-                    for (int i = 0; i < n; i++) energy.push_back(getValidatedInt());
-                    cout << "Enter initial energy: ";
-                    int initialEnergy = getValidatedInt();
-                    cout << "Maximum cities that can be visited: "
-                         << EfficientEnergyCalculation(energy, initialEnergy) << endl;
-                    break;
-                }
-
-                case 2: {
-                    vector<int> arr;
-                    cout << "Enter number of elements: ";
-                    int n = getValidatedInt();
-                    cout << "Enter elements: ";
-                    for (int i = 0; i < n; i++) arr.push_back(getValidatedInt());
-                    int index = suffix_prefix(arr);
-                    if (index != -1)
-                        cout << "Index where prefix = suffix: " << index << endl;
-                    else
-                        cout << "No such index found.\n";
-                    break;
-                }
-
-                case 3: {
-                    vector<int> switches;
-                    cout << "Enter number of binary values: ";
-                    int n = getValidatedInt();
-                    cout << "Enter 0s and 1s: ";
-                    for (int i = 0; i < n; i++) switches.push_back(getValidatedInt());
-                    cout << "Minimum flips required: " << flips(switches) << endl;
-                    break;
-                }
-
-                case 4: {
-                    vector<int> elements;
-                    cout << "Enter number of elements: ";
-                    int n = getValidatedInt();
-                    cout << "Enter elements: ";
-                    for (int i = 0; i < n; i++) elements.push_back(getValidatedInt());
-                    cout << "The odd one out is: " << OddOneOut(elements) << endl;
-                    break;
-                }
-
-                case 5: {
-                    vector<int> nums;
-                    cout << "Enter number of elements: ";
-                    int n = getValidatedInt();
-                    cout << "Enter elements: ";
-                    for (int i = 0; i < n; i++) nums.push_back(getValidatedInt());
-                    cout << "Enter target sum: ";
-                    int target = getValidatedInt();
-                    if (smartPairFinder(nums, target))
-                        cout << "Pair found with the given target sum.\n";
-                    else
-                        cout << "No pair found.\n";
-                    break;
-                }
-
-                case 6: {
-                    vector<int> time;
-                    cout << "Enter number of walls: ";
-                    int n = getValidatedInt();
-                    cout << "Enter painting times: ";
-                    for (int i = 0; i < n; i++) time.push_back(getValidatedInt());
-                    cout << "Minimum time to paint all (skipping one): "
-                         << MinimalistPainter(time) << endl;
-                    break;
-                }
-
-                default:
-                    cout << "Invalid choice! Try again.\n";
-                    break;
-            }
-        } catch (const invalid_argument& e) {
-            cout << e.what() << endl;
-            cout << "Please try again.\n";
         }
     }
+
+    cout << "Number of subarrays with sum " << target << " = " << count << endl;
 
     return 0;
 }
--- Task2.cpp
+++ Task2.cpp
@@ -1,109 +1,36 @@
+/*
+ * @filename - Task2.cpp
+ * @description - This program performs Shifting Cipher operation.
+ * @author - Aditi Agrawal
+ */
+
 #include <iostream>
-#include <cmath>
-#include <iomanip>
+#include <string>
 using namespace std;
 
-//Base class
-class Point {
-private:
-    float x, y;  
-
-public:
-    // Constructor Overloading
-    Point() : x(0), y(0) {}
-    Point(float x, float y) {
-        this->x = x;
-        this->y = y;
-    }
-
-    // Distance between two points
-    float distance(Point other) {
-        return sqrt(pow(x - other.x, 2) + pow(y - other.y, 2));
-    }
-
-    // Translate the point by dx and dy
-    void translate(float dx, float dy) {
-        x += dx;
-        y += dy;
-    }
-
-    // Display coordinates
-    void display() const {
-        cout << "(" << fixed << setprecision(1) << x << ", " << y << ")";
-    }
-};
-
-//Base Class: Shape 
-class Shape {
-public:
-    // Virtual method (to be overridden)
-    virtual float get_area() const = 0;
-};
-
-//Derived Class: Circle 
-class Circle : public Shape {
-private:
-    float radius;
-
-public:
-    Circle(float radius) {
-        this->radius = radius;
-    }
-
-    // Override get_area()
-    float get_area() const override {
-        return M_PI * radius * radius;
-    }
-};
-
-// Derived Class: Square 
-class Square : public Shape {
-private:
-    float side;
-
-public:
-    Square(float side) {
-        this->side = side;
-    }
-
-    // Override get_area()
-    float get_area() const override {
-        return side * side;
-    }
-};
-
-//Main Function
 int main() {
-    float x1, y1, x2, y2, radius, side, dx, dy;
+    string s;
+    cout << "Enter the string on which you want to perform shifting cipher: ";
+    getline(cin, s);
 
-    // Input
-    cout<<"Enter coordintes of point1 (x1,y1) : ";
-    cin >> x1 >> y1 ;
+    int k;
+    cout << "Enter value by which you want to shift: ";
+    cin >> k;
 
-    cout<<"Enter coordinates of point2 (x2,y2) : ";
-    cin >> x2 >> y2 ;
-    cout<<"Enter radius of circle: ";
-    cin >> radius;
-    cout<<"Enter side of square: ";
-    cin >> side;
-    cout<<"Enter translation values (dx ,dy) : ";
-    cin >> dx >> dy;
+    // To handle large k values we calculate modulus (cyclic)
+    k = k % 26;
 
-    // Create objects
-    Point p1(x1, y1), p2(x2, y2);
-    Circle c(radius);
-    Square s(side);
+    for (int i = 0; i < s.size(); i++) {
+        char ch = s[i];
 
-    // Compute and print
-    cout << fixed << setprecision(2);
-    cout << "Distance: " << p1.distance(p2) << endl;
-    cout << "Circle Area: " << c.get_area() << endl;
-    cout << "Square Area: " << s.get_area() << endl;
+        if (ch >= 'A' && ch <= 'Z') {  
+            s[i] = 'A' + (ch - 'A' + k) % 26;
+        } 
+        else if (ch >= 'a' && ch <= 'z') {
+            s[i] = 'a' + (ch - 'a' + k) % 26;
+        }
+    }
 
-    p1.translate(dx, dy);
-    cout << "Translated Point1: ";
-    p1.display();
-    cout << endl;
-
+    cout << "The string after shifting cipher operation will be: " << s << endl;
     return 0;
 }
