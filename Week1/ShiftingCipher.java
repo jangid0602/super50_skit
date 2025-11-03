06536daa1b7a7caa21f760fdf67328aaee2bb619
Aditya Virmani <adityavirmani4125@gmail.com>
2025-11-03 14:22:27 +0530

    First question

---
+++ Week1/ShiftingCipher.java
@@ -0,0 +1,12 @@
+public class ShiftingCipher {
+    public static void main(String[] args) {
+        String s="Abc! z";
+        int k=2;
+        String output="";
+        for(int i=0;i<s.length();i++){
+            if(s[i]>=65 && s[i]<=90){
+                
+            }
+        }
+    }
+}
