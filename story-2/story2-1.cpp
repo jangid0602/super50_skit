bccd99ee2528092597507227ae375b050b61d099
disha872 <dishatoshniwal4@gmail.com>
2025-11-10 11:20:15 +0530

    Add files via upload

---
+++ story-2/story2-1.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+int calculateTrendsetterScore(vector<int>& postIDs, int windowSize) {
+    int totalPosts = postIDs.size();
+    if (windowSize > totalPosts) return 0;
+
+    unordered_map<int, int> frequencyMap;
+    int trendsetterScore = 0;
+
+    for (int i = 0; i < windowSize; i++) frequencyMap[postIDs[i]]++;
+
+    auto hasUniqueTopPost = [&]() {
+        int highestFrequency = 0, highestCount = 0;
+        for (auto &entry : frequencyMap) {
+            if (entry.second > highestFrequency) {
+                highestFrequency = entry.second;
+                highestCount = 1;
+            } else if (entry.second == highestFrequency) {
+                highestCount++;
+            }
+        }
+        return highestCount == 1;
+    };
+
+    if (hasUniqueTopPost()) trendsetterScore++;
+
+    for (int i = windowSize; i < totalPosts; i++) {
+        frequencyMap[postIDs[i - windowSize]]--;
+        if (frequencyMap[postIDs[i - windowSize]] == 0) frequencyMap.erase(postIDs[i - windowSize]);
+        frequencyMap[postIDs[i]]++;
+
+        if (hasUniqueTopPost()) trendsetterScore++;
+    }
+
+    return trendsetterScore;
+}
+
+int main() {
+    int totalPosts, windowSize;
+    cout << "Enter number of posts: ";
+    cin >> totalPosts;
+    vector<int> postIDs(totalPosts);
+    cout << "Enter post IDs: ";
+    for (int i = 0; i < totalPosts; i++) cin >> postIDs[i];
+
+    cout << "Enter window size k: ";
+    cin >> windowSize;
+
+    cout << "Trendsetter score: " << calculateTrendsetterScore(postIDs, windowSize) << endl;
+    return 0;
+}
