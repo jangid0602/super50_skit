a6edaac6625fc4fb65bddea17ae341b34754a1cd
Priyanshu-Joshi01 <priyanshu052004@gmail.com>
2026-01-11 22:55:06 +0530

    Add files via upload

---
+++ Story4/Main.java
@@ -0,0 +1,149 @@
+// @filename - Main.java
+// @description -Recursion & Backtracking
+// @author - Priyanshu Joshi
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            try {
+                System.out.println("\nSelect a problem to solve:");
+                System.out.println("1. Russian Doll Mystery");
+                System.out.println("2. Staircase Architect");
+                System.out.println("3. Digital Ancestry Tree");
+                System.out.println("4. Word Mirror");
+                System.out.println("5. Maze Runner");
+                System.out.println("6. Perfect Team Assembly");
+                System.out.println("7. Secure Vault Code");
+                System.out.println("8. String Tiling Challenge");
+                System.out.println("9. Budget Calculator");
+                System.out.println("10. Project Builder Blueprint");
+                System.out.println("11. Exit");
+                System.out.print("Enter choice: ");
+
+                int ch = sc.nextInt();
+
+                switch (ch) {
+
+                    case 1:
+                        System.out.print("Enter doll size: ");
+                        int size = sc.nextInt();
+                        System.out.println(Method.findKey(size) ? "Key Found!" : "Key Not Found");
+                        break;
+
+                    case 2:
+                        System.out.print("Enter steps: ");
+                        int n = sc.nextInt();
+                        System.out.println("Ways = " + Method.countWays(n));
+                        break;
+
+                    case 3:
+                        System.out.print("Enter wealth: ");
+                        int wealth = sc.nextInt();
+                        System.out.print("Enter number of children: ");
+                        int c = sc.nextInt();
+                        int children[] = new int[c];
+                        for (int i = 0; i < c; i++)
+                            children[i] = sc.nextInt();
+                        System.out.println("Total Legacy = " +
+                                Method.calculateLegacy(wealth, children, 0));
+                        break;
+
+                    case 4:
+                        sc.nextLine();
+                        System.out.print("Enter word: ");
+                        String word = sc.nextLine();
+                        System.out.println(Method.isPalindrome(word, 0, word.length() - 1));
+                        break;
+
+                    case 5:
+                        System.out.print("Enter maze size: ");
+                        int m = sc.nextInt();
+                        int maze[][] = new int[m][m];
+                        for (int i = 0; i < m; i++)
+                            for (int j = 0; j < m; j++)
+                                maze[i][j] = sc.nextInt();
+                        System.out.println(Method.pathExists(maze, 0, 0));
+                        break;
+
+                    case 6:
+                        System.out.print("Enter number of developers: ");
+                        int d = sc.nextInt();
+                        sc.nextLine();
+                        String devs[] = new String[d];
+                        for (int i = 0; i < d; i++)
+                            devs[i] = sc.nextLine();
+
+                        ArrayList<ArrayList<String>> subsets = new ArrayList<>();
+                        Method.generateSubsets(devs, 0, new ArrayList<>(), subsets);
+                        System.out.println(subsets);
+                        break;
+
+                    case 7:
+                        System.out.print("Enter number of digits: ");
+                        int digitLength = sc.nextInt();
+
+                        int arr[] = new int[digitLength];
+                        for (int i = 0; i < digitLength; i++) {
+                            arr[i] = sc.nextInt();
+                        }
+
+                        ArrayList<String> result = new ArrayList<>();
+                        Method.generatePermutations(arr, new boolean[digitLength], new StringBuilder(), result);
+                        System.out.println(result);
+                        break;
+
+                    case 8:
+                        sc.nextLine();
+                        System.out.print("Enter string: ");
+                        String s = sc.nextLine();
+                        System.out.println(Method.compressString(s, 0));
+                        break;
+
+                    case 9:
+                        System.out.print("Enter number of items: ");
+                        int it = sc.nextInt();
+                        int price[] = new int[it];
+                        for (int i = 0; i < it; i++)
+                            price[i] = sc.nextInt();
+                        System.out.print("Enter budget: ");
+                        int b = sc.nextInt();
+
+                        ArrayList<ArrayList<Integer>> combos = new ArrayList<>();
+                        Method.combinationSum(price, b, 0, new ArrayList<>(), combos);
+                        System.out.println(combos);
+                        break;
+
+                    case 10:
+                        sc.nextLine();
+                        System.out.print("Enter number of tasks: ");
+                        int t = sc.nextInt();
+                        sc.nextLine();
+                        String tasks[] = new String[t];
+                        for (int i = 0; i < t; i++)
+                            tasks[i] = sc.nextLine();
+
+                        System.out.println("Tasks to do : ");
+                        System.out.println("");
+
+                        Method.printTasks(tasks, 0, 0);
+                        break;
+
+                    case 11:
+                        System.out.println("Exiting...");
+                        return;
+
+                    default:
+                        System.out.println("Invalid choice!");
+                }
+            } catch (Exception e) {
+                System.out.println("Invalid input!");
+                sc.nextLine();
+            }
+        }
+    }
+}
---
+++ Story4/Method.java
@@ -0,0 +1,163 @@
+// @filename - Method.java
+// @description -Recursion & Backtracking
+// @author - Priyanshu Joshi
+
+import java.util.*;
+
+public class Method {
+
+    // 1.Russian Doll Mystery
+    static boolean findKey(int size) {
+        if (size == 1) {
+            return true;
+        }
+
+        return findKey(size - 1);
+    }
+
+    // 2.Staircase Architect
+    static int countWays(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        return countWays(n - 1) + countWays(n - 2);
+    }
+
+    // 3.Digital Ancestry Tree
+    static int calculateLegacy(int wealth, int children[], int idx) {
+
+        if (idx == children.length) {
+            return wealth;
+        }
+
+        return calculateLegacy(wealth + children[idx], children, idx + 1);
+    }
+
+    // 4. Word Mirror
+    static boolean isPalindrome(String s, int l, int r) {
+
+        // base case
+        if (l >= r) {
+            return true;
+        }
+        // if characters at l and r are not equal
+        if (s.charAt(l) != s.charAt(r)) {
+            return false;
+        }
+
+        return isPalindrome(s, l + 1, r - 1);
+    }
+
+    // 5.Maze Runner
+    static boolean pathExists(int maze[][], int i, int j) {
+
+        int n = maze.length;
+        // out of bounds or blocked cell
+        if (i < 0 || j < 0 || i >= n || j >= n || maze[i][j] == 0) {
+            return false;
+        }
+        // reached destination
+        if (i == n - 1 && j == n - 1) {
+            return true;
+        }
+        // mark as visited
+        maze[i][j] = 0;
+
+        return pathExists(maze, i + 1, j) || pathExists(maze, i, j + 1);
+
+    }
+
+    // 6.Perfect Team Assembly
+    static void generateSubsets(String arr[], int idx, ArrayList<String> temp, ArrayList<ArrayList<String>> result) {
+
+        if (idx == arr.length) {
+            result.add(new ArrayList<>(temp));
+            return;
+        }
+        // exclude
+        generateSubsets(arr, idx + 1, temp, result);
+
+        // include
+        temp.add(arr[idx]);
+        generateSubsets(arr, idx + 1, temp, result);
+        temp.remove(temp.size() - 1);
+    }
+
+    // 7.Secure Vault Code
+    static void generatePermutations(int arr[], boolean visit[], StringBuilder sb, ArrayList<String> result) {
+
+        if (sb.length() == arr.length) {
+            result.add(sb.toString());
+            return;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+
+                // include
+                sb.append(arr[i]);
+
+                // exclude
+                generatePermutations(arr, visit, sb, result);
+
+                // backtrack
+                sb.deleteCharAt(sb.length() - 1);
+                visit[i] = false;
+            }
+        }
+
+    }
+
+    // 8.String Tiling Challenge
+    static String compressString(String s, int idx) {
+        if (idx == s.length()) {
+            return "";
+        }
+
+        char ch = s.charAt(idx);
+        int count = 1;
+        int i = idx + 1;
+
+        while (i < s.length() && s.charAt(i) == ch) {
+            count++;
+            i++;
+        }
+
+        return count + "" + ch + compressString(s, i);
+    }
+
+    // 9. Budget Calculator
+    static void combinationSum(int arr[], int target, int idx, ArrayList<Integer> temp,
+            ArrayList<ArrayList<Integer>> result) {
+
+        if (target == 0) {
+            result.add(new ArrayList<>(temp));
+            return;
+        }
+
+        if (idx == arr.length || target < 0)
+            return;
+
+        temp.add(arr[idx]);
+        combinationSum(arr, target - arr[idx], idx, temp, result);
+        temp.remove(temp.size() - 1);
+
+        combinationSum(arr, target, idx + 1, temp, result);
+    }
+
+    // 10.Project Builder Blueprint
+    static void printTasks(String tasks[], int idx, int level) {
+        if (idx == tasks.length) {
+            return;
+        }
+
+        for (int i = 0; i < level; i++)
+            System.out.print("  ");
+
+        System.out.println(tasks[idx]);
+        printTasks(tasks, idx + 1, level + 1);
+
+    }
+}
