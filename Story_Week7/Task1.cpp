6a86ccf485bb17a32015d894a0020bcf314ef295
Ayushi Shukla <ayushishukla543@gmail.com>
2026-02-01 18:26:30 +0530

    Add files via upload

---
+++ Story_Week7/Task1.cpp
@@ -0,0 +1,36 @@
+/**
+ * @filename - Task1.cpp
+ * @description - Startup revenue Maxima
+ * @author - Ayushi Shukla
+ */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int memoization(vector<int> &revenue, int days, int index, vector<int> &dp)
+{
+    if (index >= days)
+        return 0;
+    if (dp[index] != -1)
+        return dp[index];
+
+    int rest = memoization(revenue, days, index + 1, dp);
+    int work = memoization(revenue, days, index + 2, dp) + (((index + 1) % 7 == 0) ? 2 * revenue[index] : revenue[index]);
+
+    return dp[index] = max(rest, work);
+}
+int main()
+{
+    int days;
+    cout << "Enter number of days: ";
+    cin >> days;
+    vector<int> revenue(days);
+    cout << "Enter revenue for " << days << " days: ";
+    for (int i = 0; i < days; i++)
+    {
+        cin >> revenue[i];
+    }
+    vector<int> dp(days, -1);
+    cout << "Maximum revenue is : " << memoization(revenue, days, 0, dp);
+    return 0;
+}
---
+++ Story_Week7/Task9.cpp
@@ -0,0 +1,55 @@
+/**
+ * @filename - Task9.cpp
+ * @description -  The Circular Office Security
+ * @author - Ayushi Shukla
+ */
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int solveRange(int startIndex, int endIndex, vector<int> &cabinValues, vector<int> &memoDP)
+{
+    if (startIndex > endIndex)
+        return 0;
+
+    if (memoDP[startIndex] != -1)
+        return memoDP[startIndex];
+
+    int takeCurrentCabin = cabinValues[startIndex] +
+                           solveRange(startIndex + 2, endIndex, cabinValues, memoDP);
+
+    int skipCurrentCabin =
+        solveRange(startIndex + 1, endIndex, cabinValues, memoDP);
+
+    return memoDP[startIndex] = max(takeCurrentCabin, skipCurrentCabin);
+}
+
+int main()
+{
+    int numberOfCabins;
+    cout << "Enter number of cabins: ";
+    cin >> numberOfCabins;
+
+    vector<int> cabinValues(numberOfCabins);
+    cout << "Enter value of each cabin: ";
+    for (int i = 0; i < numberOfCabins; i++)
+    {
+        cin >> cabinValues[i];
+    }
+
+    if (numberOfCabins == 1)
+    {
+        cout << "Maximum value that can be audited: " << cabinValues[0];
+        return 0;
+    }
+
+    vector<int> memoCase1(numberOfCabins, -1);
+    vector<int> memoCase2(numberOfCabins, -1);
+
+    int maximumAuditedValue = max(
+        solveRange(0, numberOfCabins - 2, cabinValues, memoCase1),
+        solveRange(1, numberOfCabins - 1, cabinValues, memoCase2));
+
+    cout << "Maximum value that can be audited: " << maximumAuditedValue;
+    return 0;
+}
