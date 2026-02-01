6101d4ff43e4244f03eff205c206c44649338561
disha872 <dishatoshniwal4@gmail.com>
2026-02-01 15:28:41 +0530

    Add files via upload

---
+++ story-7/Task-4.cpp
@@ -0,0 +1,41 @@
+/**
+ * @filename - Task-4.cpp
+ * @description - Placement Marathon 
+ * @author - Disha Toshniwal
+ */
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+int longestSubsequence(vector<int>& arr, int k) {
+    unordered_map<int,int> dp;
+    int ans = 0;
+
+    for(int x : arr) {
+        int prev1 = dp.count(x - k) ? dp[x - k] : 0;
+        int prev2 = dp.count(x + k) ? dp[x + k] : 0;
+
+        dp[x] = 1 + max(prev1, prev2);
+        ans = max(ans, dp[x]);
+    }
+
+    return ans;
+}
+
+int main() {
+    int N;
+    cout << "Enter size: ";
+    cin >> N;
+
+    vector<int> arr(N);
+    cout << "Enter elements: ";
+    for(int i = 0; i < N; i++) cin >> arr[i];
+
+    int k;
+    cout << "Enter k: ";
+    cin >> k;
+
+    cout << longestSubsequence(arr, k);
+    return 0;
+}
